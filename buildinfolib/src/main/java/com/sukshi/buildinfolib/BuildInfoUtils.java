7cd465c358d0e1f74a84835dd6e35b02be9ba010
Jeevan Deshmukh <jeevan@sukshi.com>
2019-07-08 15:59:03 +0530

    progaurd release

--- buildinfolib/src/main/java/com/sukshi/buildinfolib/BuildInfoUtils.java
+++ buildinfolib/src/main/java/com/sukshi/buildinfolib/BuildInfoUtils.java
@@ -4,6 +4,7 @@ public class BuildInfoUtils {
 
     public String getBuildInfoString(){
         System.out.println("From buildinfoutils aar");
+         String uri = BuildConfig.SomeURL;
         return "This is called from buildinfoutils aar";
     }
 
